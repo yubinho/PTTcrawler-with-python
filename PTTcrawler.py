5e0f5df24f48745ada86b99a0d243d70d97d37c0
yubinho <44282084+yubinho@users.noreply.github.com>
2022-04-28 22:32:00 +0800

    Add files via upload

--- PTTcrawler.py
+++ PTTcrawler.py
@@ -1,26 +1,32 @@
+
 import urllib.request as req
 import bs4
 import csv
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+import jieba
 
-
-def GetData(url, postDate):
+def GetData(url, postDate,userAgent):
     # create request object including Request Headers to pretend browser
     request = req.Request(url, headers={
         "cookie": "over18=1",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"})
+        "User-Agent": userAgent})
     with req.urlopen(request) as response:
         data = response.read().decode("utf-8")
     # parse html
     soup = bs4.BeautifulSoup(data, "html.parser")  # parse html format by bs4
     titles = soup.find_all("div", class_="title")  # find title class
-    file = open("./wordtext/data4.txt", "w", encoding="utf-8")
+    file = open("data.txt", "a", encoding="utf-8")
+    isContinue = False
     for title in titles:
         if title.a != None:  # exclude those posts have been deleted
             date = soup.find("div", class_="date")
-            newdate = date.text
-            newdate = newdate.replace(" ", "")
+            newdate = date.text.replace(" ", "")
             if newdate == str(postDate):  # judge post date
                 print(title.a.string)
+                isContinue = True
             # store to txt
                 file.write(str(title.a.string))
                 # # put data into csv
@@ -28,18 +34,60 @@ def GetData(url, postDate):
                 #     writer = csv.writer(csvfile, delimiter=' ')
                 #     writer.writerow(str(title.a.string))
     file.close()
-    # 抓取上一頁的連結
+
     nextLink = soup.find("a", string="‹ 上頁")  # catch url of last page
     # print(nextLink["href"])
-    return nextLink["href"]
+    if(isContinue):
+        return nextLink["href"]
+    else:
+        return None
+
+def create_cloudword():
+    with open('resource/stopword.txt', 'r', encoding='utf-8') as file:  # customized illegal word
+        stops = file.read().split('\n')
+
+    text_from_file_with_apath = open(
+        'data.txt', encoding='utf8').read()  # data 
+
+    breakword = jieba.cut(
+        text_from_file_with_apath, cut_all=False)  # default hyphenation
+
+    final_words = []  # legal word
+
+    for word in breakword:
+        if word not in stops: 
+            final_words.append(word)
+
+    wl_space_split = " ".join(final_words)
+
+    font_path = 'resource/irohamaru-Regular.ttf'  # font
+
+    abel_mask = np.array(Image.open("resource/heart.jpg"))  # image outline
+
+    my_wordcloud = WordCloud(background_color='white', mask=abel_mask,
+                            font_path=font_path).generate(wl_space_split)  # create wordcloud
+    plt.figure(figsize=(10, 10))
+    plt.imshow(my_wordcloud)
+    plt.axis("off")
+    plt.show()
+
+if __name__ == '__main__':
+    # catch pages
+    userAgent = input("please input user agent of browser\n")
+    postDate = input("please input post date ex.4/13\n")
+    boardName = input("please input post date ex.Gossiping\n")
+    PageURL = 'https://www.ptt.cc/bbs/'+boardName+'/index.html'
+
+    while not GetData(PageURL, postDate,userAgent) == None:  # finding all posts of assigned date
+        PageURL = 'https://www.ptt.cc'+GetData(PageURL, postDate,userAgent)
+
+    create_cloudword()
 
 
-# catch pages
-postDate = input("please input post date ex.4/13\n")
-boardName = input("please input post date ex.Gossiping\n")
-PageURL = 'https://www.ptt.cc/bbs/'+boardName+'/index.html'
-#PageURL = 'https://www.ptt.cc/bbs/Gossiping/index.html'
-count = 0
-while count < 10:  # finding page setting
-    PageURL = 'https://www.ptt.cc'+GetData(PageURL, postDate)
-    count += 1
+    # count = 0
+    # while count < 5:
+    #     PageURL = 'https://www.ptt.cc'+GetData(PageURL, postDate,userAgent)
+    #     count+=1
+    
+
+
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# PTTcrawler-with-python
+# Intro
+這是一款以Python為開發基礎的爬蟲程式，供使用者針對網頁版PTT進行標題爬蟲，並以文字雲顯示最終資訊。
+
+# Requirements
+python套件
+* bs4
+* numpy
+* Pillow
+* matplotlib
+* wordcloud
+* jieba
+
+# Usage
+
+1. ```
+   python PTTcrawler.py
+   ```
+
+2. 輸入瀏覽器的user agent [點此查詢](https://www.whatismybrowser.com/detect/what-is-my-user-agent/)
+
+    ![This is a alt text.](/image/userAgent.png)
+
+3. 輸入文章日期
+    
+    ![This is a alt text.](/image/date.png)
+4. 輸入看板名稱
+    
+    ![This is a alt text.](/image/board.png)
+
+5. 顯示結果
+    ![This is a alt text.](/image/result.png)
